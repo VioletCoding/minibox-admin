72e2d085b920bc82d18ae733f2461322373163b9
Violet <1054197367@qq.com>
2021-01-30 20:05:53 +0800

    管理员告一段落

--- src/views/admin/index/main/user/MyPersonalCenter.vue
+++ src/views/admin/index/main/user/MyPersonalCenter.vue
@@ -10,55 +10,43 @@
                         :wrapper-col="{span:8}">
             <a-form-model-item label="昵称"
                                prop="nickname">
-              <a-input placeholder="给自己起个名字"/>
+              <a-input placeholder="给自己起个名字"
+                       v-model="dataSource.nickname"
+                       @change="saveVisible = true"/>
             </a-form-model-item>
             <a-form-model-item label="个性签名"
                                prop="description">
               <a-textarea placeholder="写点关于自己的事儿吧"
+                          v-model="dataSource.description"
+                          @change="saveVisible = true"
                           autoSize/>
             </a-form-model-item>
             <a-form-model-item label="个人等级">
-              <a-input disabled value="Lv 1"/>
+              <span>Lv {{ dataSource.level }}</span>
             </a-form-model-item>
             <a-form-model-item label="登陆密码">
               <a>点击修改</a>
+            </a-form-model-item>
+            <a-form-model-item v-if="saveVisible"
+                               label="操作">
+              <a-button type="primary"
+                        @click="save">保存
+              </a-button>
             </a-form-model-item>
           </a-form-model>
         </a-col>
 
         <a-col :span="8">
-          <a-upload :before-upload="beforeUpload">
-            <img src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-                 style="width: 300px;height: 300px;border-radius: 50%"/>
+          <a-upload :action="action"
+                    :headers="headers"
+                    name="multipartFile"
+                    @change="afterUpload"
+                    list-type="picture">
+            <img :src="dataSource.userImg"
+                 style="width: 300px;height:300px;object-fit: cover;border-radius: 50%"/>
           </a-upload>
         </a-col>
       </a-row>
-      <!--使用VueCropper进行图片裁剪-->
-      <a-modal :visible="modalVisible"
-               @cancel="modalVisible=false"
-               width="1200px" :bodyStyle="{height:'500px'}">
-        <div style="display: flex;flex-direction: row">
-          <div style="width: 800px;height: 500px;">
-            <VueCropper
-                ref="cropper"
-                :img="cropperBase64"
-                :autoCrop="true"
-                :fixedBox="true"
-                autoCropWidth="500"
-                autoCropHeight="300"
-                :centerBox="true"
-                mode="cover"
-                @realTime="realTime"/>
-          </div>
-          <!--裁剪预览图-->
-          <div :style="previewStyle">
-            <div :style="previews.div">
-              <img :src="previews.url"
-                   :style="previews.img"/>
-            </div>
-          </div>
-        </div>
-      </a-modal>
 
     </div>
     <div v-else>
@@ -69,62 +57,58 @@
 
 <script>
 import MyLoading from "@/component/MyLoading";
-import { VueCropper } from 'vue-cropper';
+import Api from "@/api/api";
+import util from "@/api/util";
 
 export default {
   name: "MyPersonalCenter",
-  components: {MyLoading, VueCropper},
+  components: {MyLoading},
+  inject: ["reload"],
   data() {
     return {
       //页面是否正在加载
       loading: false,
-      //裁剪弹出框是否可见
-      modalVisible: false,
-      //用于裁剪的图片的base64
-      cropperBase64: "",
-      //预览
-      previews: {},
-      //预览的样式
-      previewStyle: {}
+      action: "http://192.168.0.105:20001/" + Api.upload,
+      headers: {
+        accessToken: util.getLoginUserToken()
+      },
+      fileList: [],
+      dataSource: {},
+      saveVisible: false
     }
   },
   methods: {
-    //上传之前
-    async beforeUpload(file) {
-      //读取文件
-      let reader = new FileReader();
-      //读取base64
-      await reader.readAsDataURL(file);
-      //在读取完成后
-      reader.onload = () => {
-        //获取读取结果并赋值用于裁剪
-        this.cropperBase64 = reader.result;
-        //弹出裁剪框
-        this.modalVisible = true;
-        //返回promise,resolve执行上传,reject停止上传
-        return new Promise((resolve, reject) => {
-
-        });
+    getUserInfo() {
+      this.$http.post(Api.getUserList, {id: util.getLoginUserId()})
+          .then(resp => this.dataSource = resp.data.data[0])
+          .catch(err => util.errMessage(err));
+    },
+    afterUpload({fileList}) {
+      if (fileList[0].status == "done") {
+        this.dataSource.userImg = fileList[0].response.data;
+        this.$http.post(Api.updateUser, {userImg: this.dataSource.userImg, id: util.getLoginUserId()})
+            .then(resp => {
+              this.$message.success(resp.data.message);
+              sessionStorage.setItem("userImg", this.dataSource.userImg);
+              localStorage.setItem("userImg", this.dataSource.userImg);
+              this.reload();
+            })
+            .catch(err => util.errMessage(err));
       }
     },
-    //实时预览被裁剪的图片
-    realTime(data) {
-      //定义一些属性
-      let previews = data;
-      //缩放比例
-      let h = 0.5;
-      //预览样式
-      this.previewStyle = {
-        width: previews.w + "px",
-        height: previews.h + "px",
-        overflow: "hidden",
-        margin: "50px",
-        //缩放
-        zoom: h
-      };
-      this.previews = previews;
+    save() {
+      this.$http.post(Api.updateUser, this.dataSource)
+          .then(resp => {
+            if (resp.data.code == 200) {
+              this.$message.success(resp.data.message);
+            } else {
+              this.$message.warning(resp.data.message);
+            }
+          }).catch(err => util.errMessage(err));
     }
-
+  },
+  mounted() {
+    this.getUserInfo();
   }
 }
 </script>
